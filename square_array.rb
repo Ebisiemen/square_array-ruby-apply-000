3fdae5b2b180a043a3363606b2755054d51d060d
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2018-12-23 18:59:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
+def 
+  numbers =[1,2,3]
+  numbers.each { |e| puts e**2 } 
+  square_array(numbers)
+  square_array.each do 
+  #=>   
 end
+
+def 
+  numbers = [1,2,3]
+  new_numbers = []
+  numbers.each do |square_array|  
+  new_numbers = numbers ** numbers 
+  #=>
+end  
+
+puts numbers  
+puts new_numbers
